4223daefb954eb67de7e18c8f5382d93586afe55
siva <sivam1@netapp.com>
2018-04-20 07:38:45 +0530

    change password integrated

--- src/app/forgot-password/reset/reset.component.ts
+++ src/app/forgot-password/reset/reset.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup ,FormControl } from '@angular/forms';
+import { AuthService } from '../../shared/auth.service';
 
 
 @Component({
@@ -10,19 +11,27 @@ import { FormGroup ,FormControl } from '@angular/forms';
 })
 export class ResetComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private authService:AuthService) { }
 
   ngOnInit() {
     
   }
   forgotPass(resetPass){
     var email=resetPass.value.email;
-    if(email=="demo2@test.com"){
-      this.router.navigate(['/thankyou']);
-    }
-    else{
-      console.log('change password fail');
-    }
+    this.authService.forgetPassword({}).subscribe(res=>{
+      if(res['status']=='Success'){
+        this.router.navigate(['/thankyou']);
+      }
+    },error=>{
+      console.log('change password fail',error);
+    });
+
+    // if(email=="demo2@test.com"){
+    //   this.router.navigate(['/thankyou']);
+    // }
+    // else{
+    //   console.log('change password fail');
+    // }
   }
   
 }
