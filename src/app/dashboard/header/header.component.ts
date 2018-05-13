b5ea5daa54f338abf4d3426c62b642f1ecc21225
siva <sivam1@netapp.com>
2018-05-14 00:32:41 +0530

    tool tip done

--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { DashboardServiceService } from '../dashboard-service.service';
-
+declare var $;
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+
+  
 
   openPopup = false;
   leaseData = {};
@@ -16,8 +18,29 @@ export class HeaderComponent implements OnInit {
     "totalDefaultBadgeCount": 0
   };
 
-  constructor(private dashboardService:DashboardServiceService) { }
+  constructor(private dashboardService:DashboardServiceService) {
 
+
+   }
+
+   ngAfterViewInit(): void {
+    
+    $('#capital-trigger').on('click touch', function(){
+      console.log('-----------clicked triger----------')
+      $('#capital-tooltip').show();
+    });         
+     
+    $(document).on('click touch', function(event) {
+      if (!$(event.target).parents().addBack().is('#capital-trigger')) {
+        $('#capital-tooltip').hide();
+      }
+    });
+
+    $('#capital-tooltip').on('click touch', function(event) {
+      event.stopPropagation();
+    });
+  }
+  
   ngOnInit() {
     this.dashboardService.changeObject.subscribe(data=>{
       if (data && data['totalBadgeCount']) {
